ef796f20363e46bc37cfb0b2bc28366482bb5fbf
Jkang1221 <jiakangphil@163.com>
2022-08-19 11:37:36 -0400

    yaml

--- springboot_03_yaml/src/main/java/com/example/controller/BookController.java
+++ springboot_03_yaml/src/main/java/com/example/controller/BookController.java
@@ -1,6 +1,9 @@
 package com.example.controller;
 
+import com.example.MyDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +17,18 @@ public class BookController {
     private String country1;
     @Value("${user1.name}")
     private String name1;
+    @Autowired
+    private Environment env;
+    @Autowired
+    private MyDataSource myDataSource;
 
     @GetMapping
     public String getById() {
         System.out.println("springboot is running");
         System.out.println("country1===>" + country1);
         System.out.println("user.name===>" + name1);
+        System.out.println("env===>" + env.getProperty("city"));
+        System.out.println("mydatasource" + myDataSource);
         return "springboot is running";
     }
 }
